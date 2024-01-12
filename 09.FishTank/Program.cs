b365746e3d84fe8562248c0257593deec39f5f1e
VenelinaP <136800304+VenelinaP@users.noreply.github.com>
2024-01-12 14:01:39 +0200

    Add files via upload

---
+++ 09.FishTank/Program.cs
@@ -0,0 +1,11 @@
+﻿int length = int.Parse(Console.ReadLine());
+int width = int.Parse(Console.ReadLine());
+int height = int.Parse(Console.ReadLine());
+double percent = double.Parse(Console.ReadLine());
+
+int tank = length * width * height;
+double volume = tank * 0.001; //0.001 - тъй като 1 литър = 1 кубичен дециметър
+double space = percent / 100.0; //свободно пространство в аквариума
+double totalLitters = volume * (1 - space);
+
+Console.WriteLine(totalLitters);
