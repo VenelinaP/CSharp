8f830be4f4a1a69a5bd49048fb007c7a3f745726
VenelinaP <136800304+VenelinaP@users.noreply.github.com>
2024-01-12 14:18:16 +0200

    Add files via upload

---
+++ 04.VacationBooksList/Program.cs
@@ -0,0 +1,7 @@
+﻿int currBookPages = int.Parse(Console.ReadLine());
+int pagesPerHour = int.Parse(Console.ReadLine());
+int days = int.Parse(Console.ReadLine());
+
+int hoursPerDay = currBookPages / (pagesPerHour * days);
+
+Console.WriteLine(hoursPerDay);
---
+++ 08.PetShop/Program.cs
@@ -0,0 +1,9 @@
+﻿
+int dogFood = int.Parse(Console.ReadLine());
+int catFood = int.Parse(Console.ReadLine());
+double sumDog = dogFood * 2.50;
+int sumCat = catFood * 4;
+double sum = sumDog + sumCat;
+
+Console.WriteLine($"{sum} lv.");
+
