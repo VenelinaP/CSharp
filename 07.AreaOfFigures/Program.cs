3a8d1768ffc30e7ebb433ab1b9eb71ea70c72f47
VenelinaP <136800304+VenelinaP@users.noreply.github.com>
2024-01-16 09:44:54 +0200

    Add files via upload

---
+++ 07.AreaOfFigures/Program.cs
@@ -0,0 +1,25 @@
+﻿string figure = Console.ReadLine();
+double num = double.Parse(Console.ReadLine());
+
+if (figure == "square")
+{
+    double area = num * num;
+    Console.WriteLine($"{area:F3}");
+}
+else if (figure == "rectangle")
+{
+    double b = double.Parse(Console.ReadLine());
+    double area = num * b;
+    Console.WriteLine($"{area:F3}");
+}
+else if (figure == "circle")
+{
+    double area = num * num * Math.PI;
+    Console.WriteLine($"{area:F3}");
+}
+else 
+{
+    double hA = double.Parse(Console.ReadLine());
+    double area = num * hA / 2;
+    Console.WriteLine($"{area:F3}");
+}
