35c1e340b2179385c49810ae70e0cef5fcc6eda9
VenelinaP <136800304+VenelinaP@users.noreply.github.com>
2024-01-23 12:56:28 +0200

    Add files via upload

---
+++ 04.PersonalTitles/Program.cs
@@ -0,0 +1,25 @@
+﻿double age = double.Parse(Console.ReadLine());
+char gender = char.Parse(Console.ReadLine()); //в char може да се пише само един символ
+
+if (age >= 16)
+{
+    if (gender == 'm')
+    {
+        Console.WriteLine("Mr.");
+    }
+    else if (gender == 'f')
+    {
+        Console.WriteLine("Ms.");
+    }
+}
+else
+{
+    if (gender == 'm')
+    {
+        Console.WriteLine("Master");
+    }
+    else if (gender == 'f')
+    {
+        Console.WriteLine("Miss");
+    }
+}
---
+++ 07.WorkingHours/Program.cs
@@ -0,0 +1,37 @@
+﻿int time = int.Parse(Console.ReadLine());
+string day = Console.ReadLine();
+
+if (time >= 10 && time < 18)
+{
+    if (day == "Monday")
+    {
+        Console.WriteLine("open");
+    }
+    else if (day == "Tuesday")
+    {
+        Console.WriteLine("open");
+    }
+    else if (day == "Wednesday")
+    {
+        Console.WriteLine("open");
+    }
+    else if (day == "Thursday")
+    {
+        Console.WriteLine("open");
+    }
+    else if (day == "Friday")
+    {
+        Console.WriteLine("open");
+    }
+    else if (day == "Saturday")
+    {
+        Console.WriteLine("open");
+    }    
+}
+else
+{
+    Console.WriteLine("closed");
+}
+
+
+
