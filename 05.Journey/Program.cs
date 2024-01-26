ee3a05950879fbcd22ba3fe5159d75b7281f095f
VenelinaP <136800304+VenelinaP@users.noreply.github.com>
2024-01-26 13:15:15 +0200

    Add files via upload

---
+++ 05.Journey/Program.cs
@@ -0,0 +1,46 @@
+﻿double budget = double.Parse(Console.ReadLine());
+string season = Console.ReadLine();
+
+string destination = "";
+string place = "";
+
+if (budget <= 100)
+{
+    destination = "Bulgaria";
+    {
+        if (season == "summer")
+        {
+            budget *= 0.30;
+            place = "Camp";
+        }
+        else if (season == "winter")
+        {
+            budget *= 0.70;
+            place = "Hotel";
+        }
+    }
+}
+else if (budget <= 1000)
+{
+    destination = "Balkans";
+    {
+        if (season == "summer")
+        {
+            budget *= 0.40;
+            place = "Camp";
+        }
+        else if (season == "winter")
+        {
+            budget *= 0.80;
+            place = "Hotel";
+        }
+    }
+}
+if (budget > 1000)
+{
+    destination = "Europe";
+    place = "Hotel";
+    budget *= 0.90;
+}
+Console.WriteLine($"Somewhere in {destination}");
+Console.WriteLine($"{place} - {budget:f2}");
---
+++ 06.OperationsBetweenNumbers/Program.cs
@@ -0,0 +1,55 @@
+﻿int n1 = int.Parse(Console.ReadLine());
+int n2 = int.Parse(Console.ReadLine());
+char c = char.Parse(Console.ReadLine());
+
+double result = 0.0;
+
+if (c == '+' || c == '-' || c =='*')
+{
+    switch (c)
+    {
+        case '+':
+            result = n1 + n2;
+            break;
+        case '-':
+            result = n1 - n2;
+            break;
+        case '*':
+            result = n1 * n2;
+            break;
+    }if (result % 2 == 0)
+    {
+        Console.WriteLine($"{n1} {c} {n2} = {result} - even");
+    }
+    else
+    {
+        Console.WriteLine($"{n1} {c} {n2} = {result} - odd");
+    }
+
+}
+else if (c == '/')
+{
+    if (n2 == 0)
+    {
+        Console.WriteLine($"Cannot divide {n1} by zero");
+    }
+    else
+    {
+        result = (double)n1 / n2;
+        Console.WriteLine($"{n1} / {n2} = {result:f2}");
+    }
+}
+else if (c == '%')
+{
+    if (n2 == 0)
+    {
+        Console.WriteLine($"Cannot divide {n1} by zero");
+    }
+    else
+    {
+        result = n1 % n2;
+        Console.WriteLine($"{n1} % {n2} = {result}");
+    }
+}
+
+
