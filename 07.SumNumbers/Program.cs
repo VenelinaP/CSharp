f8cfb9fe1f51205f1fa0d5a20b1b03db4011d460
VenelinaP <136800304+VenelinaP@users.noreply.github.com>
2024-01-30 09:43:47 +0200

    Add files via upload

---
+++ 07.SumNumbers/Program.cs
@@ -0,0 +1,9 @@
+﻿int num = int.Parse(Console.ReadLine());
+int sum = 0;
+
+for (int i = 0; i < num; i++)
+{
+    int currNum = int.Parse(Console.ReadLine());
+    sum += currNum;
+}
+Console.WriteLine(sum);
