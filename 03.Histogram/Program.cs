a8bcefca07fa5c857ad813cce12ad1a4abc9b652
VenelinaP <136800304+VenelinaP@users.noreply.github.com>
2024-01-31 14:49:35 +0200

    Add files via upload

---
+++ 03.Histogram/Program.cs
@@ -0,0 +1,44 @@
+﻿double n = double.Parse(Console.ReadLine());
+
+int c1 = 0;
+int c2 = 0;
+int c3 = 0;
+int c4 = 0;
+int c5 = 0;
+
+for (int i = 1;  i <= n; i++)
+{
+    int num = int.Parse(Console.ReadLine());
+
+    if (num < 200)
+    {
+        c1++;
+    }
+    else if (num <= 399)
+    {
+        c2++;
+    }
+    else if (num <= 599)
+    {
+        c3++;
+    }
+    else if (num <= 799)
+    {
+        c4++;
+    }
+    else
+    {
+        c5++;
+    } 
+}
+double p1 = (c1 / n) * 100;
+double p2 = (c2 / n) * 100;
+double p3 = (c3 / n) * 100;
+double p4 = (c4 / n) * 100;
+double p5 = (c5 / n) * 100;
+
+Console.WriteLine($"{p1:f2}%");
+Console.WriteLine($"{p2:f2}%");
+Console.WriteLine($"{p3:f2}%");
+Console.WriteLine($"{p4:f2}%");
+Console.WriteLine($"{p5:f2}%");
---
+++ 04.CleverLily/Program.cs
@@ -0,0 +1,32 @@
+﻿int age = int.Parse(Console.ReadLine());
+double washerPrice = double.Parse(Console.ReadLine());
+double pricePerToy = double.Parse(Console.ReadLine());
+
+double money = 0.0;
+double moneyGiven = 10.0;
+
+for (int birthday = 1; birthday <= age; birthday++)
+{
+    if (birthday % 2 == 0)
+    {
+        money += moneyGiven - 1;
+        moneyGiven += 10;
+    }
+    else
+    {
+        money += pricePerToy;
+    }
+}
+if (money >= washerPrice)
+{
+    double moneyLeft = money - washerPrice;
+    Console.WriteLine($"Yes! {moneyLeft:f2}");
+}
+else
+{
+    double moneyNeeded = washerPrice - money;
+    Console.WriteLine($"No! {moneyNeeded:f2}");
+}
+
+
+
