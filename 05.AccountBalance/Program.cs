bf2ac9db35edf34191ccb6a0e5188b98aa67fa83
VenelinaP <136800304+VenelinaP@users.noreply.github.com>
2024-02-06 19:58:36 +0200

    Add files via upload

---
+++ 05.AccountBalance/Program.cs
@@ -0,0 +1,20 @@
+﻿string input = Console.ReadLine();
+
+double bankAccount = 0;
+
+while(input != "NoMoreMoney")
+{
+    double currAmount = double.Parse(input); //парсваме числото от инпут, защото вее сме го подали от конзолата
+
+    if (currAmount < 0) //по условие не може да се въвеждат отрицателни числа, затова правим тази проверка, дало числото е -
+    {
+        Console.WriteLine("Invalid operation!");
+        break;
+    }
+
+    bankAccount += currAmount; //добавяме сума към текущата сметка
+    Console.WriteLine($"Increase: {currAmount:f2}");
+
+    input = Console.ReadLine();
+}
+Console.WriteLine($"Total: {bankAccount:f2}"); //принтираме тотал
---
+++ 06.MaxNumber/Program.cs
@@ -0,0 +1,17 @@
+﻿string input = Console.ReadLine();
+
+int maxNum = int.MinValue; //когато търсим най-голямото число не започваме от 0, а от MinValue.
+
+while (input != "Stop")
+{
+    
+    int num = int.Parse(input);
+
+    if (num > maxNum)
+    {
+        maxNum = num;
+    }
+
+    input = Console.ReadLine();
+}
+Console.WriteLine(maxNum);
