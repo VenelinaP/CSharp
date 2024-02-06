22c7d5e28043b18d98752eef3081547051212823
VenelinaP <136800304+VenelinaP@users.noreply.github.com>
2024-02-06 19:59:51 +0200

    Add files via upload

---
+++ 07.MinNumber/Program.cs
@@ -0,0 +1,17 @@
+﻿string input = Console.ReadLine();
+
+int minNum = int.MaxValue; //когато търсим най-малкото число не започваме от 0, а от MaxValue.
+
+while (input != "Stop")
+{
+
+    int num = int.Parse(input);
+
+    if (num < minNum)
+    {
+        minNum = num;
+    }
+
+    input = Console.ReadLine();
+}
+Console.WriteLine(minNum);
---
+++ 08.Graduation/Program.cs
@@ -0,0 +1,36 @@
+﻿string name = Console.ReadLine();
+
+int grade = 1; //създаваме променлива за класа
+double allGrades = 0.0; //създаваме променлива за всички оценки, които после ще добавяме към класа
+int badGrades = 0;//променлива за лошите оценки
+
+//въртим цикъл за класовете, докато е по-малък/= 12, прави нещо
+while (grade <= 12)
+{
+    double currGrades = double.Parse(Console.ReadLine());//започваме да четем оценките
+
+    if (currGrades < 4.00)
+    {
+        badGrades++;
+
+        if (badGrades < 2)//ако получи две лоши оценки ще бъде изключен
+        {
+            continue;//иначе продължаваме програмата като въвеждаме нова оценка
+        }
+        else
+        {
+            Console.WriteLine($"{name} has been excluded at {grade} grade");
+            break;
+        }
+    }
+
+    grade++; //увеличаваме класа, ако оценката е повече от 4
+    allGrades += currGrades; 
+}
+if (grade >= 12) //тъй като този изход е само ако е стигнал до 12 клас, ако е изключен програмата тр да спре
+{
+    double averageGrades = allGrades / 12; //изчисляваме средната оценка
+    Console.WriteLine($"{name} graduated. Average grade: {averageGrades:f2}");
+}
+
+
