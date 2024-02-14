30839a14b8a9df7630fe7c5871699353caafa4c2
VenelinaP <136800304+VenelinaP@users.noreply.github.com>
2024-02-14 13:22:18 +0200

    Add files via upload

---
+++ 03.SumPrimeNonPrime/Program.cs
@@ -0,0 +1,38 @@
+﻿string input = Console.ReadLine(); //четем променливата от конзолата
+
+//създаваме променливи за сумите на простите и непростите числа
+int primeSum = 0;
+int nonPrimeSum = 0;
+
+//въртим цикъл до команда стоп
+while (input != "stop")
+{
+    int num = int.Parse(input); //променлива за нов вход преобразуван от стринг към число
+    
+    if (num < 0) //проверка дали числото е отрицателно
+    {
+        Console.WriteLine("Number is negative.");
+        input = Console.ReadLine(); //четем нов вход
+        continue; //продължаваме към следващото завъртане на цикъла ако числото не е отрицателно
+    }
+    bool isPrime = true; //променлива за просто число
+    
+        for (int i = 2; i < num; i++) //цикъл докато стигнем до просто число
+        {
+            if (num % i == 0) //проверка дали числото е просто
+            {
+                isPrime = false;
+            }                 
+        }
+
+        if (isPrime) //ако е просто увеличаваме сумата
+           primeSum += num;
+        else
+            nonPrimeSum += num;
+           
+    input = Console.ReadLine(); //четем нов вход             
+    }
+    Console.WriteLine($"Sum of all prime numbers is: {primeSum}");
+    Console.WriteLine($"Sum of all non prime numbers is: {nonPrimeSum}");
+
+
---
+++ 06.CinemaTickets/Program.cs
@@ -0,0 +1,42 @@
+﻿//създаваме променливи за:
+int studentTickets = 0; //за общия брой продадени стандартни билети
+int standardTickets = 0; //за общия брой продадени студентски билети
+int kidTickets = 0; //за общия брой продадени детски билети
+
+string filmName = Console.ReadLine(); //променлива за вход от конзолата
+
+//завъртаме цикъл до команда "Finish"
+while (filmName != "Finish")
+{
+    int freeTickets = int.Parse(Console.ReadLine()); //=>четем броя на свободните места
+
+    string ticketType = string.Empty; //четем от к. 
+    int buyTickets = 0;
+
+    //=>вътрешен цикъл докато закупените билети са по-малко от свободните места
+    while (buyTickets < freeTickets)
+    {
+        ticketType = Console.ReadLine();
+        if (ticketType == "End") //при команда "End" прекратяваме цикъла
+            break;
+
+        buyTickets++; //увеличаваме брояча на закупените билети с 1   
+
+        if (ticketType == "student") //==>проверяваме какъв билет е продаден
+            studentTickets++; //==>ако е "student" => увеличаваме общия брой продадени студ.билети
+        else if (ticketType == "standard")
+            standardTickets++; //==>ако е "stандард" => увеличаваме общия брой продадени станд.билети
+        else 
+            kidTickets++; //==>ако е "кид" => увеличаваме общия брой продадени детски билети
+    }
+    //=>отпечатваме:
+    Console.WriteLine($"{filmName} - {(double)buyTickets / freeTickets * 100:f2}% full.");
+    filmName = Console.ReadLine(); //=>четем ново име на филма
+}
+//намираме общия брой билети за всички прожекции и отпечатваме:
+int totalTickets = studentTickets + standardTickets + kidTickets;
+
+Console.WriteLine($"Total tickets: {totalTickets}");
+Console.WriteLine($"{(double)studentTickets / totalTickets * 100:f2}% student tickets.");
+Console.WriteLine($"{(double)standardTickets / totalTickets * 100:f2}% standard tickets.");
+Console.WriteLine($"{(double)kidTickets / totalTickets * 100:f2}% kids tickets.");
